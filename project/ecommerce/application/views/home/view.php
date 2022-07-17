47a42c106abfd9bee5ce64c03a3d4e4fd7bf4944
DaoCaoDuy <donnydvn19@gmail.com>
2022-07-17 10:12:36 +0700

    fixing css

--- project/ecommerce/application/views/home/view.php
+++ project/ecommerce/application/views/home/view.php
@@ -9,36 +9,36 @@
      
         <?php echo '<image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'shipping.png"' . '" alt="' . '"/ >'; ?>
         <?php echo '<image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'banner.jpeg"' . '" alt="' . '"/ >'; ?>
-        <h1>#MOISale</h1>
-        <?php echo '<a href="?url=category/view/top/coat"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'men.jpeg"' . '" alt="' . '"/ ></a>'; ?>
+        <h1 style="color:#21618C ;font-size:70px;font-weight:bold;text-align:center;">#MOISale</h1>
+        <?php echo '<a href="?url=category/view/top/coat"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'menbanner.jpg"' . '" alt="' . '"/ ></a>'; ?>
         <?php echo '<a href="?url=category/view/shoes"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'banner-shoes.jpeg"' . '" alt="' . '"/ ></a>'; ?>
-        <h1>#MOIProducts</h1>
+        <h1 style="color:#21618C ;font-size:70px;font-weight:bold;text-align:center;">#MOIProducts</h1>
         <table id="home-table">
             <tr>
                 <td><?php echo '<a href="?url=category/view/top/t-shirt"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 't-shirt.jpeg"' . '" alt="' . '"/ ></a>'; ?></td>
                 <td class=" td-padding">
-                    <h1>2022 New T-shirt Collection</h1>
-                    <h2>Active and Creative</h2>
+                    <h1 style="color:#2E86C1;text-align:center;">2022 New T-shirt Collection</h1>
+                    <h2 style="color:black;font-weight:bold;text-align:center;" >Active and Creative</h2>
                 </td>
             </tr>
             <tr>
                 <td class="td-right td-padding">
-                    <h1>Straight-Fit Jeans</h1>
-                    <h2>Elevate your look with the latest MOI's jeans</h2>
+                    <h1 style="color:#2E86C1;text-align:center;">Straight-Fit Jeans</h1>
+                    <h2 style="color:black;font-weight:bold;text-align:center;">Elevate your look with the latest MOI's jeans</h2>
                 </td>
                 <td><?php echo '<a href="?url=category/view/bottom/jeans"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'jeans.jpeg"' . '" alt="' . '"/ ></a>'; ?></td>
             </tr>
             <tr>
                 <td><?php echo '<a href="?url=category/view/shoes"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'shoes3.jpeg"' . '" alt="' . '"/ ></a>'; ?></td>
                 <td class=" td-padding">
-                    <h1>Unifactor Running Shoes</h1>
-                    <h2>All styles and colours available in the online store.</h2>
+                    <h1 style="color:#2E86C1;text-align:center;">Unifactor Running Shoes</h1>
+                    <h2 style="color:black;font-weight:bold;text-align:center;">All styles and colours available in the online store</h2>
                 </td>
             </tr>
             <tr>
                 <td class="td-right td-padding">
-                    <h1>2022 Summer Collection</h1>
-                    <h2>Available online and in stores until 22/7/2022</h2>
+                    <h1 style="color:#2E86C1;text-align:center;">2022 Summer Collection</h1>
+                    <h2 style="color:black;font-weight:bold;text-align:center;">Available online and in stores until 22/7/2022</h2>
                 </td>
                 <td><?php echo '<a href="?url=category/view/top/coat"><image class="img-max-width" src="' . PATH_URL_IMG_HOME . 'coat.png"' . '" alt="' . '"/ ></a>'; ?></td>
             </tr>
